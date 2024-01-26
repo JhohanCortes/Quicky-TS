49a021be08877cdbb474721e6861b6bf3ec5c74f
JhohanCortes <dicenadie2@gmail.com>
2024-01-25 23:03:53 -0500

    low changes i am tired

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 <header>
   {/* <!-- Navigation bar --> */}
   <nav
-    className="relative flex w-full items-center justify-between bg-primary py-2 text-white shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+    className="relative flex w-full items-center justify-between bg-primary py-2 text-white shadow-lg hover:gray focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
     data-te-navbar-ref>
     <div className="flex w-full flex-wrap items-center justify-between px-3">
       <div className="flex items-center">
